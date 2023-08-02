45de26177cfe30f28564d960fc43d88a301d7382
Théo Le Goc <theo@legoc.net>
2023-08-02 18:02:15 +0200

    Added create_user test function, get all users and get user functions

---
+++ get_all_users/app.py
@@ -0,0 +1,18 @@
+import json
+import boto3
+import os
+# import requests
+
+
+def lambda_handler(event, context):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(os.environ['USER_TABLE'])
+
+    # Get all users
+    response = table.scan()
+
+    # Create response
+    return {
+        "statusCode": 200,
+        "body": json.dumps(response['Items'])
+    }
---
+++ get_user/app.py
@@ -0,0 +1,29 @@
+import json
+import boto3
+import os
+# import requests
+
+
+def lambda_handler(event, context):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(os.environ['USER_TABLE'])
+
+    # Get user from event.pathParameters.username
+    response = table.get_item(
+        Key={
+            'username': event['pathParameters']['username']
+        }
+    )
+
+    # Create response, if empty return 404
+    if 'Item' not in response:
+        return {
+            "statusCode": 404,
+            "body": json.dumps({"error": "User not found"})
+        }
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps(response['Item'])
+    }
+
