1be2bfc8cac0a5e1b5035f2f52a24a928a181f59
Tom Chauvel <35542432+AquaBx@users.noreply.github.com>
2024-09-15 17:03:03 +0200

    ok

--- reset_password/app.py
+++ reset_password/app.py
@@ -16,7 +16,7 @@ def lambda_handler(event, context):
 
         return {
             'statusCode': 200,
-            'body': json.dumps({"message": 'Challenge removed from todo'}),
+            'body': json.dumps({"message": 'Password Changed'}),
             'headers': {
                 'Access-Control-Allow-Origin': '*'
             }
