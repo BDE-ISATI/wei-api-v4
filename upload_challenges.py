837a430de8f0560e81529fdc5573f2ad8e064792
Florian Cliquet <156716505+FlorianCliquet@users.noreply.github.com>
2025-09-01 21:50:02 +0200

    update csv

--- upload_challenges.py
+++ upload_challenges.py
@@ -8,39 +8,38 @@ dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('wei_app_db_challenges')
 
 challenges = pd.read_csv('defis.csv')
-
 for chall in challenges.values:
     print(chall)
 
     if (chall[4] == 'Défi Permanent' or chall[4] == 'Défi permanent'):
         # Start is 11/09/2023 at 12:00:00
-        s = '11/09/2023 12:00:00'
+        s = '08/09/2025 12:00:00'
         # End is 07/10/2023 at 00:00:00
-        e = '07/10/2023 00:00:00'
+        e = '05/09/2025 00:00:00'
     elif (chall[4] == 'semaine 1'):
-        s = '11/09/2023 12:00:00'
-        e = '17/09/2023 23:59:59'
+        s = '08/09/2025 12:00:00'
+        e = '14/09/2025 23:59:59'
     elif (chall[4] == 'semaine 1,2'):
-        s = '11/09/2023 12:00:00'
-        e = '24/09/2023 23:59:59'
+        s = '08/09/2025 12:00:00'
+        e = '21/09/2025 23:59:59'
     elif (chall[4] == 'semaine 2'):
-        s = '18/09/2023 00:00:00'
-        e = '24/09/2023 23:59:59'
+        s = '15/09/2025 00:00:00'
+        e = '21/09/2025 23:59:59'
     elif (chall[4] == 'semaine 2,3'):
-        s = '18/09/2023 00:00:00'
-        e = '01/10/2023 23:59:59'
+        s = '15/09/2025 00:00:00'
+        e = '24/09/2025 23:59:59'
     elif (chall[4] == 'semaine 3'):
-        s = '25/09/2023 00:00:00'
-        e = '01/10/2023 23:59:59'
+        s = '22/09/2025 00:00:00'
+        e = '28/09/2025 23:59:59'
     elif (chall[4] == 'semaine 3,4'):
-        s = '25/09/2023 00:00:00'
-        e = '08/10/2023 23:59:59'
+        s = '22/09/2025 00:00:00'
+        e = '05/10/2025 23:59:59'
     elif (chall[4] == 'semaine 4'):
-        s = '02/10/2023 00:00:00'
-        e = '06/10/2023 23:59:59'
+        s = '29/09/2025 00:00:00'
+        e = '05/10/2025 23:59:59'
     else:
-        s = '11/09/2023 12:00:00'
-        e = '07/10/2023 00:00:00'
+        s = '08/09/2025 12:00:00'
+        e = '05/10/2025 00:00:00'
 
     start = int(time.mktime(datetime.datetime.strptime(s, "%d/%m/%Y %H:%M:%S").timetuple()))
     end = int(time.mktime(datetime.datetime.strptime(e, "%d/%m/%Y %H:%M:%S").timetuple()))
